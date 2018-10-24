61e99dc3aa906d1e390e0b048640334d07ede8cf
Ignácio Espinoso Ribeiro <ignacioespinoso@gmail.com>
2018-10-24 09:51:22 -0300

    Adding soe pzazz to the blog app

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
+from django.utils import timezone
 from django.views import generic
 from .models import Post
 
@@ -10,8 +11,14 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_posts'
 
     def get_queryset(self):
-        return Post.objects.order_by('-pub_date')[:5]
+        return Post.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Post
     template_name = 'blog/detail.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(pub_date__lte=timezone.now())
+
